6d319242060aa4c78d596dba93989b5b8f4b3d2e
sspahic2 <spahic.sabahudin1@gmail.com>
2022-11-28 00:38:38 -0800

    Added file store url.

--- values/GlobalValues.ts
+++ values/GlobalValues.ts
@@ -1,5 +1,5 @@
 const InstagramLink: string = 'https://www.instagram.com/';
 const DetailsLink: string = 'details/';
 const CookieName: string = 'Sarajevoin-Auth';
-const ImageStorage: string = 'https://sarajevoin.ba/public';
+const ImageStorage: string = process.env.FILE_URL ?? "";
 export { InstagramLink, DetailsLink, CookieName, ImageStorage };
