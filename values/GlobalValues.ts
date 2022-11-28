cc82cc4826ed18f68064efa46360837ab8b69f3f
sspahic2 <spahic.sabahudin1@gmail.com>
2022-11-28 00:42:50 -0800

    Added file url fallback.

--- values/GlobalValues.ts
+++ values/GlobalValues.ts
@@ -1,5 +1,5 @@
 const InstagramLink: string = 'https://www.instagram.com/';
 const DetailsLink: string = 'details/';
 const CookieName: string = 'Sarajevoin-Auth';
-const ImageStorage: string = process.env.FILE_URL ?? "";
+const ImageStorage: string = process.env.FILE_URL ?? "https://store.sarajevoin.ba/public";
 export { InstagramLink, DetailsLink, CookieName, ImageStorage };
