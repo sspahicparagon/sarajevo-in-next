2bb058c65bf9f661d52e412ca8abf81195caa39b
sspahic2 <spahic.sabahudin1@gmail.com>
2022-12-11 15:46:25 -0800

    Possible fix for prisma migration issue

--- prisma/migrations/20221211232549_alter_description_column_length/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `Description` on the `location` table. The data in that column could be lost. The data in that column will be cast from `VarChar(2048)` to `VarChar(1024)`.
-
-*/
--- AlterTable
-ALTER TABLE `location` MODIFY `Description` VARCHAR(1024) NULL;
---
+++ prisma/migrations/20221211234038_drop_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Description` on the `location` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `location` DROP COLUMN `Description`;
