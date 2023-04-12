b0141f796702b6417dce7c7e878d6ffc6e0ff350
unknown <sspahic2@etf.unsa.ba>
2023-04-12 02:11:30 +0200

    Switched to Nexus for downloading json translation files

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'de', 'bs']
+
+module.exports = { 
+    i18n : {
+    defaultLocale: 'en',
+    locales: ['en', 'de', 'bs']
     }
 };
